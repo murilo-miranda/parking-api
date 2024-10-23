26eddcfbe28ff40241f4f8cd7b2a691c561585ce
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-10-23 04:28:34 -0300

    Parking model
    - Add gems to create tests
    - Implement model test
    - Create the parking model

---
+++ app/models/parking.rb
@@ -0,0 +1,4 @@
+class Parking < ApplicationRecord
+  validates :plate, presence: true, length: { is: 8 }, format: { with: /\A[A-Z]{3}-\d{4}\z/ }
+  validates :paid, inclusion: { in: [true, false] }
+end
