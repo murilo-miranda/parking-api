27811ff52e3680320e2845899a74bb3996ecf919
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-10-23 17:11:39 -0300

    Leave parking
    - Create tests for service responsible to handle parking leaving
    - Implement service responsible to handle parking leaving
    - Fix wrong bussiness logic created to avoid paying twice

---
+++ app/services/parking_service/leave.rb
@@ -0,0 +1,11 @@
+class ParkingService::Leave
+  def initialize(parking_payment_params)
+    @id = parking_payment_params[:id]
+  end
+
+  def call
+    parking = Parking.find(@id)
+    parking.update!(exit_time: DateTime.now)
+    parking
+  end
+end
---
+++ spec/services/parking_service/leave_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe ParkingService::Leave do
+  describe '.call' do
+    subject { ParkingService::Leave.new(parking_payment_params).call }
+
+    context 'successfully' do
+      let!(:parking) { Parking.create(plate: 'ABC-1234', entry_time: DateTime.now, paid: true) }
+      let(:parking_payment_params) { { id: parking.id } }
+
+      it 'update parking exit time' do
+        expect { subject }.to change { Parking.last.exit_time }.from(nil).to(within(1.minute).of(DateTime.now))
+      end
+    end
+
+    context 'parking not found' do
+      let!(:parking) { Parking.create(plate: 'ABC-1234', entry_time: DateTime.now, paid: true) }
+      let(:parking_payment_params) { { id: 999_999 } }
+
+      it 'raise error' do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'parking not payed' do
+      let!(:parking) { Parking.create(plate: 'ABC-1234', entry_time: DateTime.now) }
+      let(:parking_payment_params) { { id: parking.id } }
+
+      it 'raise error' do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+end
